d091cac1e37d86fee97b578884f5f732d6495735
LordButley <joshjwwilson@gmail.com>
2023-04-19 17:14:58 +0000

    add rest_auth apps and serializers.py in drf_api

--- profiles/views.py
+++ profiles/views.py
@@ -1,57 +1,118 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
-    List all profiles
-    No Create view (post method), as profile creation handled by django signals
+    List all profiles.
+    No create view as profile creation is handled by django signals.
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    serializer_class = ProfileSerializer
 
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
+# class ProfileList(APIView):
+#     """
+#     List all profiles
+#     No Create view (post method), as profile creation handled by django signals
+#     """
+#     filter_backends = [
+#         filters.OrderingFilter
+#     ]
+#     ordering_fields = [
+#         'posts_count',
+#         'followers_count',
+#         'following_count',
+#         'owner__following__created_at',
+#         'owner__followed__created_at',
+#     ]
 
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, context={'request': request}
-        )
-        return Response(serializer.data)
+#     def get_queryset(self, request):
+#         queryset = Profile.objects.all()
+#         return filters.BaseFilterBackend.filter_queryset(self, request, queryset, ProfileList)
 
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def get(self, request):
+#         # profiles = Profile.objects.all()
+#         queryset = Profile.objects.annotate(
+#             posts_count=Count('owner__post', distinct=True),
+#             followers_count=Count('owner__followed', distinct=True),
+#             following_count=Count('owner__following', distinct=True)
+#         ).order_by('-created_at')
+#         serializer = ProfileSerializer(
+#             queryset, many=True, context={'request': request}
+#         )
+   
+#         return Response(serializer.data)
 
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+
+# class ProfileDetail(APIView):
+#     serializer_class = ProfileSerializer
+#     permission_classes = [IsOwnerOrReadOnly]
+
+#     def get_object(self, pk):
+#         try:
+#             profile = Profile.objects.get(pk=pk)
+#             self.check_object_permissions(self.request, profile)
+#             return profile
+#         except Profile.DoesNotExist:
+#             raise Http404
+
+#     def get(self, request, pk):
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(
+#             profile, context={'request': request}
+#         )
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(
+#             profile, data=request.data, context={'request': request}
+#         )
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         post = self.get_object(pk)
+#         post.delete()
+#         return Response(
+#             status=status.HTTP_204_NO_CONTENT
+#         )
+
