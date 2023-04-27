a372bf47c7a61121e04bd9aeaab685e00194f417
LordButley <joshjwwilson@gmail.com>
2023-04-27 09:09:57 +0000

    update allowed_host

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -66,7 +66,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://jwmoments.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'https://jwmoments.herokuapp.com/']
 
 
 # Application definition
